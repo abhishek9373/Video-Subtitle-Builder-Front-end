3ca24a4ab427dfdb0d87fd705704a3e95d578e9a
abhishek9373 <abhishekgund500@gmail.com>
2024-01-08 02:43:45 +0530

    updated serverurl

--- src/shared/constants/routes.constants.js
+++ src/shared/constants/routes.constants.js
@@ -1,4 +1,4 @@
-export const SERVER_URL = "http://localhost:3015";
+export const SERVER_URL = "https://devbuilder.tech/newservices";
 export const ONBAORD_USER = SERVER_URL + "/user";
 export const VIDEO_UPLOAD = SERVER_URL + "/file";
 
